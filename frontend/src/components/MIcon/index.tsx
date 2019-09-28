e0b33ea657037acd39cfffd0a312151a4aa201c9
lxxxv5 <1490769790@qq.com>
2019-09-28 23:01:32 +0800

    chore(MIcon): add component MIcon

---
+++ frontend/src/components/MIcon/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Icon } from 'antd'
+
+const MIcon = ({ type }) => {
+    return <Icon type={type} style={{ color: 'rgba(0,0,0,.25)' }} />
+}
+
+export default MIcon
