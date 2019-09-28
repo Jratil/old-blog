66b3db0bcd6feafa493cf614c4495500df53678b
lxxxv5 <1490769790@qq.com>
2019-09-28 23:03:12 +0800

    refactor(login): refactor page login by React Hooks

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
+import React from 'react'
 import { router } from 'umi'
 import { Dispatch } from 'redux'
-import { connect } from 'dva'
-import { Form, Input, Icon, Button } from 'antd'
+import { Form, Input, Button } from 'antd'
 import { FormComponentProps } from 'antd/lib/form'
+import { MIcon } from '@/components'
 
 import styles from './index.less'
 
-interface XProps extends FormComponentProps {
+interface MProps extends FormComponentProps {
     dispatch: Dispatch
 }
 
@@ -15,46 +15,46 @@ const FormItem = Form.Item
 
 const rules = { account: [{ required: true, message: '请输入账号' }], password: [{ required: true, message: '请输入密码' }] }
 
-@connect()
-class Login extends React.Component<XProps> {
-    handleSubmit = evt => {
+const Login: React.FC<MProps> = props => {
+    const { dispatch, form } = props
+    const { getFieldDecorator } = form
+
+    const handleSubmit = evt => {
         evt.preventDefault()
-        this.props.form.validateFields((err, payload) => {
-            if (!err) this.props.dispatch({ type: 'login/login', payload })
+        form.validateFields((err, payload) => {
+            if (!err) dispatch({ type: 'login/login', payload })
         })
     }
-    handelRegister = evt => {
+
+    const handelRegister = evt => {
         evt.preventDefault()
         router.push('/register')
     }
-    render() {
-        const { getFieldDecorator } = this.props.form
 
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <FormItem>
-                    {getFieldDecorator('account', {
-                        rules: rules.account,
-                        validateTrigger: 'onBlur'
-                    })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Account"></Input>)}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: rules.password,
-                        validateTrigger: 'onBlur'
-                    })(<Input type="password" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Password"></Input>)}
-                </FormItem>
-                <FormItem>
-                    <div className={styles.btnWrapper}>
-                        <Button type="primary" onClick={this.handleSubmit}>
-                            登录
-                        </Button>
-                        <Button onClick={this.handelRegister}>注册</Button>
-                    </div>
-                </FormItem>
-            </Form>
-        )
-    }
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormItem>
+                {getFieldDecorator('account', {
+                    rules: rules.account,
+                    validateTrigger: 'onBlur'
+                })(<Input prefix={<MIcon type="user" />} placeholder="Account"></Input>)}
+            </FormItem>
+            <FormItem>
+                {getFieldDecorator('password', {
+                    rules: rules.password,
+                    validateTrigger: 'onBlur'
+                })(<Input type="password" prefix={<MIcon type="lock" />} placeholder="Password"></Input>)}
+            </FormItem>
+            <FormItem>
+                <div className={styles.btnWrapper}>
+                    <Button type="primary" onClick={handleSubmit}>
+                        登录
+                    </Button>
+                    <Button onClick={handelRegister}>注册</Button>
+                </div>
+            </FormItem>
+        </Form>
+    )
 }
 
 export default Form.create()(Login)
