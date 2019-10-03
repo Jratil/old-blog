4ff618f56f3f45035301fe88ebf474cc112fedee
lxxxv5 <1490769790@qq.com>
2019-10-03 22:05:20 +0800

    style: modify MIcon

--- frontend/src/components/MIcon/index.tsx
+++ frontend/src/components/MIcon/index.tsx
@@ -4,11 +4,7 @@ import icons from '@/icons/index.ts'
 
 const MIcon = ({ type, style = {}, ...props }) => {
     const isCustom = Object.keys(icons).includes(type)
-    const tempStyle = {
-        color: 'rgba(0,0,0,.25)',
-        ...style
-    }
-    return <Icon type={isCustom || type} style={tempStyle} component={isCustom && icons[type]} {...props} />
+    return <Icon type={isCustom || type} style={style} component={isCustom && icons[type]} {...props} />
 }
 
 export default MIcon
